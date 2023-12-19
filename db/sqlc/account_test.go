37bac41532fc4b2e05f101d4d8511e553f738027
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2023-12-19 14:44:37 -0500

    test get account successfully completed

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,11 +4,12 @@ import (
 	"avancedGo/util"
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccoutn(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwnerName(),
 		Balance:  util.RandomMoney(),
@@ -24,5 +25,27 @@ func TestCreateAccoutn(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccoutn(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+
+	randomAccount1 := createRandomAccount(t)
+	randomAccount2, err := testQueries.GetAccount(context.Background(), randomAccount1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, randomAccount2)
+
+	require.Equal(t, randomAccount1.ID, randomAccount2.ID)
+	require.Equal(t, randomAccount1.Balance, randomAccount2.Balance)
+	require.Equal(t, randomAccount1.Currency, randomAccount2.Currency)
+	require.Equal(t, randomAccount1.Owner, randomAccount2.Owner)
+	require.Equal(t, randomAccount1.Currency, randomAccount2.Currency)
+
+	require.WithinDuration(t, randomAccount1.CreatedAt, randomAccount2.CreatedAt, time.Second)
 
 }
