2e80ef785c5c8935b957aa1eecd9ecf2315d1392
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2023-12-22 01:14:51 -0500

    Function to execute transactions done

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
 type Store struct {
 	DB *sql.DB
@@ -12,4 +16,22 @@ func NewStore(db *sql.DB) *Store {
 		DB:      db,
 		Queries: New(db),
 	}
+}
+
+// Function to execute a transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	queries := New(tx)
+
+	if err = fn(queries); err != nil {
+		if errRollback := tx.Rollback(); errRollback != nil {
+			return fmt.Errorf("have been ocurred some errors during transaction and rollback: %v, %v", err, errRollback)
+		}
+		return err
+	}
+	return tx.Commit()
 }
