acdca5358e34f4b31a5a846bfe5aeda6a7604bd1
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2023-12-22 00:56:27 -0500

    typo fixed from entries sql file

--- db/query/entries.sql
+++ db/query/entries.sql
@@ -1,25 +1,25 @@
---name: CreateEntry :one
+-- name: CreateEntry :one
 INSERT INTO entries (
     account_id, amount
 ) VALUES (
     $1, $2
 ) RETURNING *;
 
---name: GetEntry :one
+-- name: GetEntry :one
 SELECT * FROM entries
 WHERE id = $1 LIMIT 1;
 
---name: ListEntries :many
+-- name: ListEntries :many
 SELECT * FROM entries
 ORDER BY id
 LIMIT $1
 OFFSET $2;
 
---name: UpdateEntry :exec
+-- name: UpdateEntry :exec
 UPDATE entries
 SET amount = $2
 WHERE id = $1;
 
---name: DeleteEntry :exec
+-- name: DeleteEntry :exec
 DELETE FROM entries
 WHERE id = $1;
