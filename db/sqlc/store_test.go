211c57c84dee070b7bf2a6a4912a9846ec14bf61
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2024-01-11 10:34:22 -0500

    accounts nomenclature updated because it was confusing

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,9 +10,9 @@ import (
 
 func TestTransferTX(t *testing.T) {
 	storeTest := NewStore(testDB)
-
-	accountFrom := createRandomAccount(t)
-	accountTo := createRandomAccount(t)
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before: ", account1.Balance, account2.Balance)
 
 	// run n concurrent transfer transactions
 	n := 5
@@ -23,14 +24,15 @@ func TestTransferTX(t *testing.T) {
 	for i := 0; i < n; i++ {
 		go func() {
 			result, err := storeTest.TransferTX(context.Background(), TransferTXParams{
-				FromAccountID: accountFrom.ID,
-				ToAccountID:   accountTo.ID,
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
 				Amount:        amount,
 			})
 			errs <- err
 			results <- result
 		}()
 	}
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -40,8 +42,8 @@ func TestTransferTX(t *testing.T) {
 
 		transfer := result.Transfer
 		require.NotEmpty(t, transfer)
-		require.Equal(t, accountFrom.ID, transfer.FromAccountID)
-		require.Equal(t, accountTo.ID, transfer.ToAccountID)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
 		require.Equal(t, amount, transfer.Amount)
 		require.NotZero(t, transfer.ID)
 		require.NotZero(t, transfer.CreatedAt)
@@ -51,7 +53,7 @@ func TestTransferTX(t *testing.T) {
 
 		fromEntry := result.FromEntry
 		require.NotEmpty(t, fromEntry)
-		require.Equal(t, accountFrom.ID, fromEntry.AccountID)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
 		require.Equal(t, -amount, fromEntry.Amount)
 		require.NotZero(t, fromEntry.ID)
 		require.NotZero(t, fromEntry.CreatedAt)
@@ -61,7 +63,7 @@ func TestTransferTX(t *testing.T) {
 
 		toEntry := result.ToEntry
 		require.NotEmpty(t, toEntry)
-		require.Equal(t, accountTo.ID, toEntry.AccountID)
+		require.Equal(t, account2.ID, toEntry.AccountID)
 		require.Equal(t, amount, toEntry.Amount)
 		require.NotZero(t, toEntry.ID)
 		require.NotZero(t, toEntry.CreatedAt)
@@ -69,5 +71,37 @@ func TestTransferTX(t *testing.T) {
 		_, err = storeTest.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
+		// check account
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		//check account balance's
+		fmt.Println(">> tx: ", fromAccount.Balance, toAccount.Balance)
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0)
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+	// check th final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after: ", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
